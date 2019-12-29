920d70918c5a80558bbcb42c61146762a8839512
Ioan Zîcu <ioan.zicu94@gmail.com>
2019-12-30 00:31:59 +0100

    list item text view

--- app/src/main/java/android/example/com/miwok/NumbersActivity.java
+++ app/src/main/java/android/example/com/miwok/NumbersActivity.java
@@ -41,7 +41,6 @@ public class NumbersActivity extends AppCompatActivity {
         // activity_numbers.xml layout file.
         ListView listView = (ListView) findViewById(R.id.list);
 
-
         // Make the {@link ListView} use the {@link ArrayAdapter} we created above, so that the
         // {@link ListView} will display list items for each word in the list of words.
         // Do this by calling the setAdapter method on the {@link ListView} object and pass in
